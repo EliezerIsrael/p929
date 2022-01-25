9585af0750710cd3aaabe225987551ba3026bcbc
Lev Israel <eliezer.israel@gmail.com>
2022-01-25 11:07:48 -0500

    v 0.6.1

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='p929',
-      version='0.6',
+      version='0.6.1',
       description='Tools for the Project 929 chapter-a-day Tanach learning project',
       py_modules=['p929'],
       author='Lev Israel',
